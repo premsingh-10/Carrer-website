2551425b83c1b748292d6fffc12895ef1a8d3900
premsingh-10 <125468003+premsingh-10@users.noreply.github.com>
2024-01-03 16:56:20 +0000

    basic layout

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
 
 @app.route("/")
-def hello_world():
-  return "Hello World!"
+def hello_World():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Job Portal')
 
 
 if __name__ == "__main__":
