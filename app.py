9360249464b4d30701f6944174737b1803f161ab
premsingh-10 <125468003+premsingh-10@users.noreply.github.com>
2024-01-09 17:06:51 +0000

    final pro

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db,add_application_to_db
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_World():
   jobs = load_jobs_from_db()
-  return render_template('home.html', 
-                jobs=jobs, 
-                  company_name='Job Portal')
-
-
+  return render_template('home.html', jobs=jobs, company_name='Job Portal')
 
 
 @app.route("/api/jobs")
@@ -24,6 +21,7 @@ def list_jobs():
 #   job = load_job_from_db(id)
 #   return jsonify(job)
 
+
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
@@ -31,9 +29,23 @@ def show_job(id):
   if not job:
     return "Not Found", 404
 
-  return render_template('jobpage.html', 
-                         job=job)
+  return render_template('jobpage.html', job=job)
 
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -33,3 +33,25 @@ def load_job_from_db(id):
           return None
       else:
           return row._asdict()
+
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+      # Use a dictionary to pass parameters to the execute method.
+      params = {
+          'job_id': job_id,
+          'full_name': data['full_name'],
+          'email': data['email'],
+          'linkedin_url': data['linkedin_url'],
+          'education': data['education'],
+          'work_experience': data['work_experience'],
+          'resume_url': data['resume_url']
+      }
+
+      # Bind the parameters using bindparams()
+      query = query.bindparams(**params)
+
+      conn.execute(query)
